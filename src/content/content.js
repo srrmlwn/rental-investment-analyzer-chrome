ef08d61cf027a9e0ffdf2eba9ebafa0adc7d6568
sriram :zap <sriram.alwan@gmail.com>
2025-05-31 18:50:52 -0700

    Removing redundant component

--- src/content/content.js
+++ src/content/content.js
@@ -118,15 +118,6 @@ class ContentScript {
                     </li>
                 </ul>
             </div>
-            <div class="ria-card">
-                <h3 class="ria-text-primary">Investment Metrics</h3>
-                <div class="ria-loading">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="ria-spinner">
-                        <path d="M12 4V2C6.48 2 2 6.48 2 12h2c0-4.42 3.58-8 8-8z" fill="currentColor"/>
-                    </svg>
-                    Not Available Yet
-                </div>
-            </div>
         `;
     }
 }
